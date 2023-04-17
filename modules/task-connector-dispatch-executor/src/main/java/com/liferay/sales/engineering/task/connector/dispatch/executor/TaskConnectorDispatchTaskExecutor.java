6bd1dc9e78c7e1dfb996cb89c5ddddd7717b8c8d
Peter Richards <peter.richards@liferay.com>
2023-04-17 10:25:54 +0100

    Added localisation to the Dispatch Executor name

--- modules/task-connector-dispatch-executor/src/main/java/com/liferay/sales/engineering/task/connector/dispatch/executor/TaskConnectorDispatchTaskExecutor.java
+++ modules/task-connector-dispatch-executor/src/main/java/com/liferay/sales/engineering/task/connector/dispatch/executor/TaskConnectorDispatchTaskExecutor.java
@@ -5,14 +5,19 @@ import com.liferay.dispatch.executor.DispatchTaskExecutor;
 import com.liferay.dispatch.executor.DispatchTaskExecutorOutput;
 import com.liferay.dispatch.model.DispatchTrigger;
 import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.sales.engineering.task.connector.api.TaskConnector;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 import java.util.Date;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 /**
  * @author peterrichards
@@ -56,7 +61,11 @@ public class TaskConnectorDispatchTaskExecutor extends BaseDispatchTaskExecutor 
 
     @Override
     public String getName() {
-        return KEY;
+        Locale locale = LocaleUtil.getDefault();
+        ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+                "content.Language", locale, getClass());
+
+        return LanguageUtil.get(resourceBundle, KEY);
     }
 
 }
